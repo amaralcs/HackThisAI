6b11629fa7e89aee615f8ceda22a7959b8365e46
amaralcs <amaralcscgp@gmail.com>
2022-08-30 18:05:01 +0100

    Add params to submission helper

--- challenge/easy_credit_check/submission_helper.py
+++ challenge/easy_credit_check/submission_helper.py
@@ -1,6 +1,17 @@
+import sys
+from argparse import ArgumentParser
 import requests
 
-# Replace `your.csv` with a datafile of your choice.
-with open("your.csv", "rb") as f:
-    r = requests.post("http://localhost:5000/check", files={"data_file": f})
-    print(r.text)
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("fpath", help="Path to file that will be submitted")
+    args = parser.parse_args(sys.argv[1:])
+    fpath = args.fpath
+
+    print(f"Sumbitting {fpath}")
+
+    # Replace `your.csv` with a datafile of your choice.
+    with open(fpath, "rb") as f:
+        r = requests.post("http://localhost:5000/check", files={"data_file": f})
+        print(r.text)
